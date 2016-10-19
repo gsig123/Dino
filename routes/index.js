8e0b9a04c69ed8b876d1ab2bead10b827df5dc60
gsig123 <sigurbergsson94@gmail.com>
2016-10-19 14:46:05 +0000

    query.js created which creates a connection to the db. test.js created to test connection by adding a test restaurant@

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var test = require('../lib/test');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  test.addRestaurant('name', 'photos', 'email', 'password', 'description', 1234567, function(err, result){
+  	if(result){
+  		console.log('Successful!');
+  	} else {
+  		console.log('Error!');
+  	}
+  });
+  
   res.render('index', { title: 'Dino' });
 });
 
