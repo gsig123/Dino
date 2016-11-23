4424659d0aba11223fca1aa61aed17ffc86b8f1c
gsig123 <sigurbergsson94@gmail.com>
2016-11-23 15:55:03 +0000

    frontendshit

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,20 +1,37 @@
-$(document).ready(function(){
-	$('.delete-offer').on('click', function(){
-		var id = $(this).data('id');
-		var url = '/users/delete' + id;
+$(document).ready(function() {
+    $('.delete-offer').on('click', function() {
+        var id = $(this).data('id');
+        var url = '/users/delete' + id;
 
-		if(confirm('Delete Offer?')){
-			$.ajax({
-				url: url, 
-				type: 'DELETE', 
-				success: function(result){
-					console.log('Deleting Offer...');
-					window.location.href = '/users/admin';
-				},
-				error: function(err){
-					console.log(err);
-				}
-			});
-		}
-	});
+        if (confirm('Delete Offer?')) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function(result) {
+                    console.log('Deleting Offer...');
+                    window.location.href = '/users/admin';
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            });
+        }
+    });
+
+    var allDays = $('#allDays');
+    var weekdays = $('.weekdays');
+    allDays.click(function() {
+    	console.log(allDays.is(':checked'));
+    	if(!(allDays.is(':checked'))) {
+    		allDays.removeAttr('checked');
+            weekdays.each(function() {
+                    $(this).removeAttr('checked');
+            });
+        } else {
+        	allDays.prop('checked', true);
+            weekdays.each(function() {
+                    $(this).attr('checked', 'checked');
+            });
+        }
+    });
 });
